8f35486c9e1900842a1b2b88d2ac3cadf49825a9
ArbazKhan5117 <arbazkhan5117@gmail.com>
2020-09-23 10:12:52 +0000

    add more componenets

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     <div>
       <h1 style={{color:"red",fontSize:"50px",fontStyle:"oblique"}}>Good Morning the whole world</h1>
       <Dinner dishName='Chicken Karahi' sweet='kheer'/>
+      <Dinner dishName='Chicken Biryani' sweet='Halwa'/>
+      <Dinner dishName='Chicken Tika' sweet='ice-cream'/>
       <h2>Arbaz Restaurant's</h2>
     </div>
   );
